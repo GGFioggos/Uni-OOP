36aabf191b48a7862c84bdf9048a4807fded3973
moschidisg <mosxidismosxidis@gmail.com>
2022-11-13 17:55:35 +0200

    add hasEnded method

--- Tic-Tac-Toe/src/Gameboard.java
+++ Tic-Tac-Toe/src/Gameboard.java
@@ -3,6 +3,7 @@ public class Gameboard {
 	private final int DIMENSIONS = 3;
 	private char[][] board = new char[DIMENSIONS][DIMENSIONS];
 
+	// PRINT CURRENT STATE OF BOARD
 	public void printBoard() {
 		System.out.println("   A B C");
 		for (int i = 0; i < DIMENSIONS; i++) {
@@ -18,11 +19,41 @@ public class Gameboard {
 		board[row][col] = symbol;
 	}
 
+	// CHECK IF BOARD BOX IS OCCUPIED
 	public boolean isOccupied(int row, int col) {
 		if (board[row][col] == 0) {
 			return false;
 		}
 		return true;
+	}
+
+	// CHECK IF GAME HAS ENDED
+	public boolean hasEnded() {
+		// CHECK HORIZONTAL
+		for (int i = 0; i < DIMENSIONS; i++) {
+			if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 0) {
+				return true;
+			}
+		}
+
+		// CHECK VERTICAL
+		for (int i = 0; i < DIMENSIONS; i++) {
+			if (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != 0) {
+				return true;
+			}
+		}
+
+		// CHECK DIAGONAL
+		if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != 0) {
+			return true;
+		}
+
+		if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != 0) {
+			return true;
+		}
+
+		return false;
+
 	}
 
 }
