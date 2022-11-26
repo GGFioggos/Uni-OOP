8f738adc4e510d43d2c703248df7525516c601e8
moschidisg <mosxidismosxidis@gmail.com>
2022-11-27 00:09:39 +0200

    add class that holds communication info

---
+++ SurveillanceNet/src/Communication.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Communication {
+	private ArrayList<String> numbers = new ArrayList<>();
+	private int year;
+	private int month;
+	private int day;
+	
+	public Communication(String number1, String number2, int day, int month, int year) {
+		this.numbers.add(number1);
+		this.numbers.add(number2);
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public void printInfo() {
+		System.out.println("Between " + numbers.get(0) + " --- " + numbers.get(1) + " on " + getFullDate());
+	}
+	
+	private String getFullDate() {
+		return (year + "/" + month + "/" + day);
+	}
+	
+	public ArrayList<String> getNumbers() {
+		return numbers;
+	}
+}
