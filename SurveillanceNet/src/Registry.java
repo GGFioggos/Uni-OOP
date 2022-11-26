7fad2093def8b9849cb1127e2d42f5c72ac8a827
moschidisg <mosxidismosxidis@gmail.com>
2022-11-27 00:11:00 +0200

    create registry class

--- SurveillanceNet/src/Registry.java
+++ SurveillanceNet/src/Registry.java
@@ -2,5 +2,88 @@ import java.util.ArrayList;
 
 public class Registry {
 	private ArrayList<Suspect> suspects = new ArrayList<>();
+	private ArrayList<Communication> communications = new ArrayList<>();
+
+	public Registry() {
+
+	}
+
+	public void addSuspect(Suspect suspect) {
+		suspects.add(suspect);
+	}
+
+	public void addCommunication(Communication communication) {
+		communications.add(communication);
+
+		Suspect sus1 = findSuspectByNumber(communication.getNumbers().get(0));
+		Suspect sus2 = findSuspectByNumber(communication.getNumbers().get(1));
+
+		sus1.addAssociate(sus2);
+		sus2.addAssociate(sus1);
+	}
+
+	private Suspect findSuspectByNumber(String number) {
+		for (Suspect suspect : suspects) {
+			for (String phonenum : suspect.getPhoneNumbers()) {
+				if (phonenum == number) {
+					return suspect;
+				}
+			}
+		}
+		return null;
+	}
+
+	public Suspect getSuspectWithMostPartners() {
+		Suspect max = suspects.get(0);
+		for (Suspect suspect : suspects) {
+			if (suspect.getPartners().size() > max.getPartners().size()) {
+				max = suspect;
+			}
+		}
+		return max;
+	}
+
+	public PhoneCall getLongestPhoneCallBetween(String number1, String number2) {
+		int max = 0;
+		PhoneCall maxCall = null;
+		for (Communication communication : communications) {
+			if (communication.getNumbers().contains(number1) && communication.getNumbers().contains(number2)) {
+				if (communication instanceof PhoneCall) {
+					PhoneCall phoneCall = (PhoneCall) communication;
+					if (phoneCall.getDuration() > max) {
+						max = phoneCall.getDuration();
+						maxCall = phoneCall;
+					}
+				}
+			}
+		}
+		return maxCall;
+	}
+
+	public ArrayList<SMS> getMessagesBetween(String number1, String number2) {
+		ArrayList<SMS> messages = new ArrayList<>();
+
+		for (Communication communication : communications) {
+			if (communication.getNumbers().contains(number1) && communication.getNumbers().contains(number2)) {
+				if (communication instanceof SMS) {
+					SMS sms = (SMS) communication;
+					if (sms.getContent().contains("Bomb") || sms.getContent().contains("Attack")
+							|| sms.getContent().contains("Explosives") || sms.getContent().contains("Gun")) {
+						messages.add(sms);
+					}
+				}
+			}
+		}
+		return messages;
+	}
 	
+	public void printSuspectsFromCountry(String country) {
+		System.out.println("Suspects coming from " + country + ":");
+		for (Suspect suspect: suspects) {
+			 if (suspect.getCountry() == country) {
+				 System.out.println(suspect.getName() + " (" + suspect.getCodeName() + ")");
+			 }
+		}
+	}
+
 }
