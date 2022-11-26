00e21f8663e5359d5adcc52023b8b92bdd84af84
moschidisg <mosxidismosxidis@gmail.com>
2022-11-27 00:10:09 +0200

    create classes that inherit from communication and represent types of communication

---
+++ SurveillanceNet/src/PhoneCall.java
@@ -0,0 +1,19 @@
+
+public class PhoneCall extends Communication {
+	private int duration;
+
+	public PhoneCall(String number1, String number2, int year, int month, int day, int duration) {
+		super(number1, number2, year, month, day);
+		this.duration = duration;
+	}
+
+	public void printInfo() {
+		System.out.println("This phone call has the following info");
+		super.printInfo();
+		System.out.println("Duration: " + duration);
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+}
---
+++ SurveillanceNet/src/SMS.java
@@ -0,0 +1,19 @@
+
+public class SMS extends Communication{
+	private String content;
+	
+	public SMS(String number1, String number2, int year, int month, int day, String content) {
+		super(number1, number2, year, month, day);
+		this.content = content;
+	}
+
+	public void printInfo() {
+		System.out.println("This SMS has the following info");
+		super.printInfo();
+		System.out.println("Text: " + content);
+	}
+	
+	public String getContent() {
+		return content;
+	}
+}
