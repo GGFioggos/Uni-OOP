e542f8c2184bdf75aac352fa2f5fc744aeeaf94f
moschidisg <mosxidismosxidis@gmail.com>
2022-12-26 22:55:38 +0200

    add window that user can search suspects

---
+++ SurveillanceNet/src/SearchGUI.java
@@ -0,0 +1,40 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SearchGUI extends JFrame {
+
+	private JTextField nameInputField = new JTextField("John Dow");
+	private JButton searchButton = new JButton("Find");
+
+	private JPanel panel = new JPanel();
+
+	public SearchGUI(Registry registry) {
+		panel.add(nameInputField);
+		panel.add(searchButton);
+
+		this.setContentPane(panel);
+
+		searchButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Suspect searchedSuspect = registry.findSuspectByName(nameInputField.getText());
+				if (searchedSuspect != null) {
+					new InfoGUI(searchedSuspect, registry);
+				} else {
+					new ErrorGUI(nameInputField.getText());
+				}
+			}
+		});
+
+		this.setSize(400, 250);
+		this.setTitle("Find Suspect");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
